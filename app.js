2db3c476e78894e38caa5e981609943bd15d26d7
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-09-15 18:04:10 -0300

    FEAT: Switched from CommonJS to ES6

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
+import express from 'express'
 
-const PORT = process.env.PORT | 3000
+const app = express()
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   console.log('request received:', req.url)
-  res.send('Hello World!')
+  res.json({ message: 'Hello World!' })
 })
 
+const PORT = process.env.PORT | 3000
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
 })
