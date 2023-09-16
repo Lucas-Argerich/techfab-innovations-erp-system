41787eddda386171885c0fb533671b47e411d801
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-09-16 14:35:51 -0300

    FEAT!: CORS milddleware & Inventory Router Implementation

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express'
+import { inventoryRouter } from './routes/inventory.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
+// Configuration
 const app = express()
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  console.log('request received:', req.url)
-  res.json({ message: 'Hello World!' })
-})
+// Middleware
+app.use(corsMiddleware())
 
+// Routes
+app.use('/inventory', inventoryRouter)
+
+// Deploy
 const PORT = process.env.PORT | 3000
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
