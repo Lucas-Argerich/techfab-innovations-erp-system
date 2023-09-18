cb02dc896c1416434e7bc2e376e182f71bc2eeb1
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-09-17 23:32:17 -0300

    IMPR: Switched Status types for enums

--- src/types/db-types.ts
+++ src/types/db-types.ts
@@ -1,12 +1,25 @@
+export enum InventoryItemStatus {
+  Available = 'available',
+  OutOfStock = 'out of stock',
+  Discontinued = 'discontinued'
+}
+
 export interface InventoryItem {
   id: number
   name: string
   category: string
   quantity: number
   price: number
+  status: InventoryItemStatus
 }
 
-export type OrderStatus = 'Pending' | 'Processing' | 'Shipped' | 'Delivered' | 'Cancelled'
+export enum OrderStatus {
+  Cancelled = 'cancelled',
+  Pending = 'pending',
+  Processing = 'processing',
+  Shipped = 'shipped',
+  Delivered = 'delivered'
+}
 
 export interface Order {
   id: number
@@ -19,7 +32,12 @@ export interface Order {
   status: string
 }
 
-export type ProductionItemStatus = 'Pending' | 'In Progress' | 'Completed' | 'Cancelled'
+export enum ProductionItemStatus {
+  Cancelled = 'cancelled',
+  Pending = 'pending',
+  InProgress = 'in progress',
+  Completed = 'completed'
+}
 
 export interface ProductionItem {
   id: number
