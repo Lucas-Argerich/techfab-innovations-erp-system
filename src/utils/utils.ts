bad9baf3e8250fbd3b495c86e2e5f44ce57ff01d
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-09-17 22:32:32 -0300

    FEAT: utils isAnyUndefined generic function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,6 @@ import { readFileSync } from 'node:fs'
 import { type Database } from '../types/db-types'
 
 export const db = JSON.parse(readFileSync('db/db.json', 'utf-8')) as Database
+
+export const isAnyUndefined = (...values: any[]): boolean =>
+  values.some((value) => value === undefined)
