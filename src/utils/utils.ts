271149703b29aa27bd32c71099786db42f54093e
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-09-18 08:19:13 -0300

    FEAT: DB States Validation Util Functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,19 @@
 import { readFileSync } from 'node:fs'
-import { type Database } from '../types/db-types'
+import { InventoryItemStatus, type Database, OrderStatus, ProductionItemStatus } from '../types/db-types'
 
 export const db = JSON.parse(readFileSync('db/db.json', 'utf-8')) as Database
 
 export const isAnyUndefined = (...values: any[]): boolean =>
   values.some((value) => value === undefined)
+
+export const isValidInventoryStatus = (status: string): status is InventoryItemStatus => {
+  return Object.values(InventoryItemStatus).includes(status as InventoryItemStatus)
+}
+
+export const isValidOrderStatus = (status: string): status is OrderStatus => {
+  return Object.values(OrderStatus).includes(status as OrderStatus)
+}
+
+export const isValidProductionItem = (status: string): status is ProductionItemStatus => {
+  return Object.values(ProductionItemStatus).includes(status as ProductionItemStatus)
+}
