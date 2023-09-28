2ee6c04217cd80033ba0e9f9ac60acbf5c2a8231
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-09-27 22:08:06 -0300

    FEAT!: employee api mvc architecture (no view yet)

---
+++ src/controllers/employees.ts
@@ -0,0 +1,118 @@
+import { type NextFunction, type Request, type Response } from 'express'
+import { employeeModel } from '../models/employee'
+import { type Employee } from '../types/db-types'
+import {
+  filterReqBody,
+  isAnyUndefined,
+  isValidEmail,
+  isValidEmployeeStatus,
+  isValidPhoneNumber
+} from '../utils/utils'
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from '../constants/messages'
+
+export const employeesController = {
+  getAll: (req: Request, res: Response, next: NextFunction) => {
+    employeeModel
+      .readAll()
+      .then((employees) => {
+        res.status(200).json(employees)
+      })
+      .catch(next)
+  },
+  getById: (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id)
+    employeeModel
+      .readById(id)
+      .then((employee) => {
+        res.status(200).json(employee)
+      })
+      .catch(next)
+  },
+  post: (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, phone, position, status }: Omit<Employee, 'id'> =
+      req.body
+
+    if (isAnyUndefined(name, email, phone, position, status)) {
+      return res.status(400).json({
+        error: ERROR_MESSAGES.INVALID_REQUEST_BODY()
+      })
+    }
+
+    if (!isValidEmail(email)) {
+      return res.status(400).json({
+        error: ERROR_MESSAGES.INVALID_EMAIL()
+      })
+    }
+
+    if (!isValidPhoneNumber(phone)) {
+      return res.status(400).json({
+        error: ERROR_MESSAGES.INVALID_PHONE()
+      })
+    }
+
+    if (!isValidEmployeeStatus(status)) {
+      return res.status(400).json({
+        error: ERROR_MESSAGES.INVALID_STATUS('employees')
+      })
+    }
+
+    const input = { name, email, phone, position, status }
+
+    employeeModel
+      .create(input)
+      .then((data) => {
+        res
+          .status(201)
+          .json({ message: SUCCESS_MESSAGES.ITEM_CREATE('employees'), data })
+      })
+      .catch(next)
+  },
+  put: (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id)
+
+    const {
+      name,
+      email,
+      phone,
+      position,
+      status
+    }: Partial<Omit<Employee, 'id'>> = req.body
+
+    if (email !== undefined && !isValidEmail(email)) {
+      return res.status(400).json({ error: ERROR_MESSAGES.INVALID_EMAIL() })
+    }
+
+    if (phone !== undefined && !isValidPhoneNumber(phone)) {
+      return res.status(400).json({ error: ERROR_MESSAGES.INVALID_PHONE() })
+    }
+
+    if (status !== undefined && !isValidEmployeeStatus(status)) {
+      return res
+        .status(400)
+        .json({ error: ERROR_MESSAGES.INVALID_STATUS('employees') })
+    }
+
+    const input = filterReqBody({ name, email, phone, position, status })
+
+    employeeModel
+      .update(id, input)
+      .then((data) => {
+        res
+          .status(200)
+          .json({ message: SUCCESS_MESSAGES.ITEM_UPDATE('employees'), data })
+      })
+      .catch(next)
+  },
+  delete: (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id)
+
+    employeeModel
+      .delete(id)
+      .then(() => {
+        res
+          .status(200)
+          .json({ message: SUCCESS_MESSAGES.ITEM_DELETE('employees') })
+      })
+      .catch(next)
+  }
+}
--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -1,152 +1,24 @@
 import { Router, json } from 'express'
-import {
-  db,
-  isAnyUndefined,
-  isValidEmail,
-  isValidEmployeeStatus,
-  isValidPhoneNumber
-} from '../utils/utils'
-import { EmployeeStatus, type Employee } from '../types/db-types'
-import { ERROR_MESSAGES, SUCCESS_MESSAGES } from '../constants/messages'
+import { employeesController } from '../controllers/employees'
+import { errorHandler } from '../middlewares/errorHandler'
 
 export const employeesRouter = Router()
 
 employeesRouter.use(json())
 
 // Get all employee items
-employeesRouter.get('/', (req, res) => {
-  res.status(200).json(db.employees)
-})
+employeesRouter.get('/', employeesController.getAll)
 
 // Get a specific employee item by ID
-employeesRouter.get('/:id', (req, res) => {
-  const itemId = parseInt(req.params.id)
-  const item = db.employees.find((item) => item.id === itemId)
-
-  if (item === undefined) {
-    return res
-      .status(404)
-      .json({ error: ERROR_MESSAGES.ITEM_NOT_FOUND('employees') })
-  }
-
-  res.status(200).json(item)
-})
+employeesRouter.get('/:id', employeesController.getById)
 
 // Create a new employee item
-employeesRouter.post('/', (req, res) => {
-  const { name, email, phone, position, status }: Employee = req.body
-
-  if (isAnyUndefined(name, email, phone, position, status)) {
-    return res.status(400).json({
-      error: ERROR_MESSAGES.INVALID_REQUEST_BODY()
-    })
-  }
-
-  if (!isValidEmail(email)) {
-    return res.status(400).json({ error: ERROR_MESSAGES.INVALID_EMAIL() })
-  }
-
-  if (!isValidPhoneNumber(phone)) {
-    return res.status(400).json({
-      error: ERROR_MESSAGES.INVALID_PHONE()
-    })
-  }
-
-  if (!isValidEmployeeStatus(status)) {
-    return res
-      .status(400)
-      .json({ error: ERROR_MESSAGES.INVALID_STATUS('employees') })
-  }
-
-  const newItem: Employee = {
-    id: db.employees.length + 1,
-    name,
-    email,
-    phone,
-    position,
-    status
-  }
-
-  db.employees.push(newItem)
-
-  res
-    .status(201)
-    .json({ message: SUCCESS_MESSAGES.ITEM_CREATE('employees'), data: newItem })
-})
+employeesRouter.post('/', employeesController.post)
 
 // Update an existing employee item
-employeesRouter.put('/:id', (req, res) => {
-  const itemId = parseInt(req.params.id)
-  const item = db.employees.find((item) => item.id === itemId)
-
-  if (item === undefined) {
-    return res
-      .status(404)
-      .json({ error: ERROR_MESSAGES.ITEM_NOT_FOUND('employees') })
-  }
-
-  const { name, email, phone, position, status }: Employee = req.body
-
-  if (name !== undefined) {
-    item.name = name
-  }
-
-  if (email !== undefined) {
-    if (isValidEmail(email)) {
-      item.email = email
-    } else {
-      return res.status(400).json({ error: ERROR_MESSAGES.INVALID_EMAIL() })
-    }
-  }
-
-  if (phone !== undefined) {
-    if (isValidPhoneNumber(phone)) {
-      item.phone = phone
-    } else {
-      return res.status(400).json({
-        error: ERROR_MESSAGES.INVALID_PHONE()
-      })
-    }
-  }
-
-  if (position !== undefined) {
-    item.position = position
-  }
-
-  if (status !== undefined) {
-    if (isValidEmployeeStatus(status)) {
-      item.status = status
-    } else {
-      return res
-        .status(400)
-        .json({ error: ERROR_MESSAGES.INVALID_STATUS('employees') })
-    }
-  }
-
-  res
-    .status(200)
-    .json({ message: SUCCESS_MESSAGES.ITEM_UPDATE('employees'), data: item })
-})
+employeesRouter.put('/:id', employeesController.put)
 
 // Terminate a specific employee item by ID
-employeesRouter.delete('/:id', (req, res) => {
-  const itemId = parseInt(req.params.id)
-  const item = db.employees.find((item) => item.id === itemId)
+employeesRouter.delete('/:id', employeesController.delete)
 
-  if (item === undefined) {
-    return res
-      .status(404)
-      .json({ error: ERROR_MESSAGES.ITEM_NOT_FOUND('employees') })
-  }
-
-  item.status = EmployeeStatus.Terminated
-
-  res
-    .status(200)
-    .json({
-      message: SUCCESS_MESSAGES.ITEM_DELETE(
-        'employees',
-        EmployeeStatus.Terminated
-      )
-    })
-})
+employeesRouter.use(errorHandler)
