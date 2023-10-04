e5abfb17fc36e90f028f35061a1ac855113f88ae
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-10-03 22:18:30 -0300

    FIX: ESLint indentation

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -9,14 +9,14 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   switch (true) {
-    case err instanceof ItemNotFound:
-      return res
-        .status(404)
-        .json({ error: ERROR_MESSAGES.ITEM_NOT_FOUND(err.typeName) })
-    default:
-      console.error('Unexpected error:', err)
-      return res
-        .status(500)
-        .json({ error: ERROR_MESSAGES.FAILED_TO(err.typeName, err.method) })
+  case err instanceof ItemNotFound:
+    return res
+      .status(404)
+      .json({ error: ERROR_MESSAGES.ITEM_NOT_FOUND(err.typeName) })
+  default:
+    console.error('Unexpected error:', err)
+    return res
+      .status(500)
+      .json({ error: ERROR_MESSAGES.FAILED_TO(err.typeName, err.method) })
   }
 }
