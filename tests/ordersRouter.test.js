fb2ec388cd8e0d99ca7756d2ecbe42c5b8268513
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-10-07 23:35:18 -0300

    FEAT: Order Item tests

--- tests/ordersRouter.test.js
+++ tests/ordersRouter.test.js
@@ -10,13 +10,19 @@ app.use(express.json())
 app.use('/orders', ordersRouter)
 
 describe('Orders API', () => {
-  let orderId // Used to store the ID of the created order
+  let orderId = 1
 
   describe('GET /orders', () => {
     it('should return all orders', async () => {
       const res = await request(app).get('/orders')
       expect(res.status).to.equal(200)
       expect(res.body).to.be.an('array')
+    })
+
+    it('should return a order by valid ID', async () => {
+      const res = await request(app).get(`/orders/${orderId}`)
+      expect(res.status).to.equal(200)
+      expect(res.body).to.be.an('object')
     })
 
     it('should return an error if the order does not exist', async () => {
@@ -114,21 +120,8 @@ describe('Orders API', () => {
 
   describe('DELETE /orders/:id', () => {
     it('should mark an order as cancelled', async () => {
-      const newOrder = {
-        customer_id: 1,
-        products: [
-          { product_id: 1, quantity: 2 },
-          { product_id: 2, quantity: 1 }
-        ],
-        total_price: 100,
-        status: OrderStatus.Pending
-      }
-
-      const createResponse = await request(app).post('/orders').send(newOrder)
-      const createdOrderId = createResponse.body.data.id
-
       const deleteResponse = await request(app).delete(
-        `/orders/${createdOrderId}`
+        `/orders/${orderId}`
       )
 
       expect(deleteResponse.status).to.equal(200)
@@ -138,6 +131,133 @@ describe('Orders API', () => {
     it('should return an error if the order does not exist', async () => {
       const invalidOrderId = 9999 // An ID that does not exist in the orders
       const res = await request(app).delete(`/orders/${invalidOrderId}`)
+      expect(res.status).to.equal(404)
+      expect(res.body).to.have.property('error')
+    })
+  })
+
+  let itemId
+
+  describe('GET /orders/:id/items', () => {
+    it('should return all the items of an order', async () => {
+      const res = await request(app).get('/orders/1/items')
+
+      expect(res.status).to.equal(200)
+      expect(res.body).to.be.an('array')
+    })
+
+    it('should return an error if the order does not exist', async () => {
+      const res = await request(app).get('/orders/999/items')
+
+      expect(res.status).to.equal(404)
+      expect(res.body).to.have.property('error')
+    })
+
+    it('should return an item of an order by valid ID', async () => {
+      const res = await request(app).get('/orders/1/items/1')
+
+      expect(res.status).to.equal(200)
+      expect(res.body).to.be.an('object')
+    })
+
+    it('should return an error if the order item does not exist', async () => {
+      const res = await request(app).get('/orders/1/items/999')
+
+      expect(res.status).to.equal(404)
+      expect(res.body).to.have.property('error')
+    })
+  })
+
+  describe('POST /orders/:id/items', () => {
+    it('should create a new item of an order', async () => {
+      const newOrderItem = {
+        quantity: 12,
+        product_id: 1
+      }
+
+      const res = await request(app).post('/orders/1/items').send(newOrderItem)
+
+      expect(res.status).to.equal(201)
+      expect(res.body).to.have.property('message')
+      expect(res.body.data).to.have.property('id')
+      expect(res.body.data).to.deep.include(newOrderItem)
+
+      itemId = res.body.data.id
+    })
+
+    it('should fail to create a new item of an order with an invalid ID', async () => {
+      const newOrderItem = {
+        quantity: 12,
+        product_id: 1
+      }
+
+      const res = await request(app).post('/orders/9999/items').send(newOrderItem)
+
+      expect(res.status).to.equal(404)
+      expect(res.body).to.have.property('error')
+    })
+
+    it('should fail to create a new item of an order with an invalid product_id', async () => {
+      const newOrderItem = {
+        quantity: 12,
+        product_id: 999
+      }
+
+      const res = await request(app).post('/orders/1/items').send(newOrderItem)
+
+      expect(res.status).to.equal(404)
+      expect(res.body).to.have.property('error')
+    })
+
+    it('should fail to create a new item of an order with missing fields', async () => {
+      const newOrderItem = {
+        quantity: 12
+      }
+
+      const res = await request(app).post('/orders/1/items').send(newOrderItem)
+
+      expect(res.status).to.equal(400)
+      expect(res.body).to.have.property('error')
+    })
+  })
+
+  describe('PUT /orders/:id/items/:itemId', () => {
+    it('should update an existing item of an order', async () => {
+      const updatedItem = {
+        quantity: 5,
+        product_id: 2
+      }
+
+      const res = await request(app).put(`/orders/1/items/${itemId}`).send(updatedItem)
+
+      expect(res.status).to.equal(200)
+      expect(res.body).to.have.property('message')
+      expect(res.body).to.have.property('data')
+    })
+
+    it('should fail to update an item of an order with an invalid product_id', async () => {
+      const updatedItem = {
+        product_id: 999
+      }
+
+      const res = await request(app).put(`/orders/1/items/${itemId}`).send(updatedItem)
+
+      expect(res.status).to.equal(404)
+      expect(res.body).to.have.property('error')
+    })
+  })
+
+  describe('DELETE /orders/:id/items/:itemId', () => {
+    it('should delete a specific item of an order by valid ID', async () => {
+      const res = await request(app).delete(`/orders/1/items/${itemId}`)
+
+      expect(res.status).to.equal(200)
+      expect(res.body).to.have.property('message')
+    })
+
+    it('should fail to delete an item of an order with an invalid ID', async () => {
+      const res = await request(app).delete(`/orders/1/items/${itemId}`)
+
       expect(res.status).to.equal(404)
       expect(res.body).to.have.property('error')
     })
